520aacb25d39d3dc21dc6c5595bae6509bf61e9c
YoshikiShiota <y.s3a6l9t@gmail.com>
2020-08-13 13:59:08 +0900

    25分間のタイマー作成

--- js/app.js
+++ js/app.js
@@ -2,8 +2,55 @@ var app = new Vue({
   el: '#app',
   data() {
     return {
-      timer: '25:00',
-      button: 'Start'
+      button: 'Start',
+      timerOn: false,
+      timer: null,
+      pickTime: null,
+      totalTime: null,
+      resetButton: false
+    }
+  },
+  methods: {
+    setTimeQuarter: function() {
+      this.pickTime = 25 * 60;
+      this.totalTime = this.pickTime;
+    },
+    start: function() {
+      this.timer = setInterval(() => this.countdown(), 1000);
+      this.timerOn = true;
+    },
+    stop: function() {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.timerOn = false;
+    },
+    resetTimer: function() {
+      this.totalTime = this.pickTime;
+      clearInterval(this.timer);
+      this.timer = null;
+      this.resetButton = false;
+    },
+    padTime: function(time) {
+      return (time < 10 ? "0" : "") + time;
+    },
+    countdown: function() {
+      if (this.totalTime >= 1) {
+        this.totalTime--;
+      } else {
+        this.totalTime = 0;
+        this.resetTimer();
+        swal("Complete!!", "", "success");
+      }
+    }
+  },
+  computed: {
+    minutes: function() {
+      const minutes = Math.floor(this.totalTime / 60);
+      return minutes;
+    },
+    seconds: function() {
+      const seconds = this.totalTime - this.minutes * 60;
+      return this.padTime(seconds);
     }
   }
 });
